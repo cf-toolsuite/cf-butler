f040a65d1db11a782a7d0026b80c43f11f929667
Chris Phillipson <cphillipson@pivotal.io>
2019-03-22 07:18:21 -0700

    Add endpoints and service implementation to obtain snapshot detail in csv format * for application and service instances

--- src/main/java/io/pivotal/cfapp/controller/SnapshotController.java
+++ src/main/java/io/pivotal/cfapp/controller/SnapshotController.java
@@ -2,6 +2,7 @@ package io.pivotal.cfapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +37,20 @@ public class SnapshotController {
 					.assembleSnapshotSummary()
 					.map(summary -> ResponseEntity.ok(summary))
 					.defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+	}
+
+	@GetMapping(value = { "/snapshot/detail/si" }, produces = MediaType.TEXT_PLAIN_VALUE )
+	public Mono<ResponseEntity<String>> getServiceInstanceCsvReport() {
+		return service
+					.assembleCsvSIReport()
+					.map(r -> ResponseEntity.ok(r));
+	}
+
+	@GetMapping(value = { "/snapshot/detail/ai" }, produces = MediaType.TEXT_PLAIN_VALUE )
+	public Mono<ResponseEntity<String>> getApplicationInstanceCsvReport() {
+		return service
+					.assembleCsvAIReport()
+					.map(r -> ResponseEntity.ok(r));
 	}
 
 }
