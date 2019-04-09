0adb866c1d157639e6169eb06d8ff0c2f5a2df35
Chris Phillipson <cphillipson@pivotal.io>
2019-04-09 05:31:44 -0700

    Add getOrganizationId to OrganizationService * and adjust formatting in other domain, service, and task impl

--- src/main/java/io/pivotal/cfapp/service/UsageService.java
+++ src/main/java/io/pivotal/cfapp/service/UsageService.java
@@ -72,15 +72,21 @@ public class UsageService {
     }
 
     public Flux<String> getTaskUsage(LocalDate start, LocalDate end) {
-        return orgService.getOrganizations().flatMap(o -> getTaskUsage(o.getId(), start, end));
+        return orgService
+                .getOrganizations()
+                    .flatMap(o -> getTaskUsage(o.getId(), start, end));
     }
 
     public Flux<String> getApplicationUsage(LocalDate start, LocalDate end) {
-        return orgService.getOrganizations().flatMap(o -> getApplicationUsage(o.getId(), start, end));
+        return orgService
+                .getOrganizations()
+                .flatMap(o -> getApplicationUsage(o.getId(), start, end));
     }
 
     public Flux<String> getServiceUsage(LocalDate start, LocalDate end) {
-        return orgService.getOrganizations().flatMap(o -> getServiceUsage(o.getId(), start, end));
+        return orgService
+                .getOrganizations()
+                    .flatMap(o -> getServiceUsage(o.getId(), start, end));
     }
 
     //----------
