0db91862ad76912725294869c344ef22114bad54
Chris Phillipson <cphillipson@pivotal.io>
2019-04-15 12:49:26 -0700

    Allow for extension of supported/known/popular buildpacks via cf.buildpacks property

--- src/main/java/io/pivotal/cfapp/config/ButlerSettings.java
+++ src/main/java/io/pivotal/cfapp/config/ButlerSettings.java
@@ -1,8 +1,11 @@
 package io.pivotal.cfapp.config;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.CollectionUtils;
@@ -16,8 +19,11 @@ public class ButlerSettings {
 
 	private static final String SYSTEM_ORG = "system";
 	private static final Set<String> DEFAULT_BLACKLIST = Set.of(SYSTEM_ORG);
+	private static final String[] KNOWN_BUILDPACKS = "apt,binary,clojure,dotnet,elixir,emberjs,erlang,go,haskell,hwc,java,jboss,jetty,liberty,meteor,nginx,nodejs,php,pyspark,python,ruby,rust,staticfile,swift,tc,tomcat,tomee,weblogic".split(",");
+	private static final Set<String> DEFAULT_BUILDPACKS = Set.of(KNOWN_BUILDPACKS);
 
-    private String apiHost;
+	private String apiHost;
+	private Set<String> buildpacks;
 	private boolean sslValidationSkipped;
 	private Integer connectionPoolSize;
     private String username;
@@ -30,17 +36,39 @@ public class ButlerSettings {
     	while (organizationBlackList.remove(""));
     	Set<String> nonEmptyBlacklist = CollectionUtils.isEmpty(organizationBlackList) ?
     			DEFAULT_BLACKLIST: organizationBlackList;
-    	return merge(nonEmptyBlacklist);
+    	return merge(nonEmptyBlacklist, DEFAULT_BLACKLIST);
 	}
 
 	public String getUsageDomain() {
 		return "https://" + getApiHost().replace("api.", "app-usage.");
 	}
 
-    private Set<String> merge(Set<String> orgBlackList) {
+	public Set<String> getBuildpacks() {
+		while (buildpacks.remove(""));
+		Set<String> nonEmptyBuildpacks = CollectionUtils.isEmpty(buildpacks) ?
+    			DEFAULT_BUILDPACKS: buildpacks;
+    	return merge(nonEmptyBuildpacks, DEFAULT_BUILDPACKS);
+	}
+
+	public String getBuildpack(String input, String image) {
+        if (!StringUtils.isBlank(image)) {
+            return null;
+        } else if (StringUtils.isBlank(input)) {
+            return "anomalous";
+        } else {
+			Optional<String> buildpack = 
+				getBuildpacks()
+					.stream()
+					.filter(b -> input.contains(b))
+					.collect(Collectors.reducing((a, b) -> null));
+            return buildpack.orElse("unknown");
+        }
+    }
+
+    private Set<String> merge(Set<String> source, Set<String> defaultList) {
     	Set<String> result = new HashSet<>();
-    	result.addAll(orgBlackList);
-    	result.addAll(DEFAULT_BLACKLIST);
+    	result.addAll(source);
+    	result.addAll(defaultList);
     	return result;
     }
 
