e73ae8e2c3105ce0d4ecd85e05de44c756677ef6
Chris Phillipson <cphillipson@pivotal.io>
2019-10-18 17:23:54 -0700

    Comment out reactor-tools dependency * Seems to interfere with application startup using cf-java-buildpack and causing     *. 2019-10-18T17:15:58.81-0700 [APP/PROC/WEB/0] ERR Caused by: java.lang.IllegalStateException: No compatible attachment provider is available    2019-10-18T17:15:58.81-0700 [APP/PROC/WEB/0] ERR     at reactor.tools.shaded.net.bytebuddy.agent.ByteBuddyAgent.install(ByteBuddyAgent.java:389)

--- src/main/java/io/pivotal/cfapp/AppInit.java
+++ src/main/java/io/pivotal/cfapp/AppInit.java
@@ -4,14 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //import reactor.blockhound.BlockHound;
-import reactor.tools.agent.ReactorDebugAgent;
+//import reactor.tools.agent.ReactorDebugAgent;
 
 
 @SpringBootApplication
 public class AppInit {
 
 	public static void main(String[] args) {
-		ReactorDebugAgent.init();
+		//ReactorDebugAgent.init();
 		//BlockHound.install();
 		SpringApplication.run(AppInit.class, args);
 	}
