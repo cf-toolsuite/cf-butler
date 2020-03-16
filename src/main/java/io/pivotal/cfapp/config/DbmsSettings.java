9b22844da5bdfb712a93aad219de086a4cc96d06
Chris Phillipson <cphillipson@pivotal.io>
2020-03-16 08:23:31 -0700

    Upgrade to Spring Boot 2.3.0.M3 (#191)
    * Add prelim Github workflow
    * Update dependencies
      * Spring Boot Experimental deps have been rolled into Spring Boot proper as of 2.3.0.M3
      * Adjust impl of CloudConfig and DbmsSettings to account for updates to R2DBCProperties and Auto-config
    * Fix actions/setup-java@v1 step, add with: reference to ${{ matrix.java }}
    * Upgrade JGit version to 5.7.0.202003110725-r

--- src/main/java/io/pivotal/cfapp/config/DbmsSettings.java
+++ src/main/java/io/pivotal/cfapp/config/DbmsSettings.java
@@ -1,14 +1,11 @@
 package io.pivotal.cfapp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.r2dbc.ConnectionFactoryAutoConfiguration;
 import org.springframework.stereotype.Component;
 
 import io.r2dbc.spi.ConnectionFactory;
 
 @Component
-@AutoConfigureAfter(ConnectionFactoryAutoConfiguration.class)
 public class DbmsSettings {
 
     private final ConnectionFactory factory;
