bbb0e3a8dae8c0eb4f1a53dad921fbe015ee7a72
Chris Phillipson <cphillipson@pivotal.io>
2020-04-30 13:40:16 -0700

    Initial support for deploying cf-butler on cf-for-k8s

---
+++ scripts/deploy.k8s.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+export APP_NAME=cf-butler
+
+if [ -z "$1" ] && [ -z "$2" ]; then
+	echo "Usage: deploy.k8s.sh {credential_store_provider_option} {path_to_secrets_file}"
+	exit 1
+fi
+
+
+
+case "$1" in
+
+	--with-credhub | -c)
+	cf push -f manifest.cf4k8s.yml --no-start
+	cf create-service credhub default $APP_NAME-secrets -c "$2"
+	cf bind-service $APP_NAME $APP_NAME-secrets
+	cf start $APP_NAME
+	;;
+
+	_ | *)
+	cf push -f manifest.cf4k8s.yml --no-start
+	cf create-user-provided-service $APP_NAME-secrets -p "$2"
+	cf bind-service $APP_NAME $APP_NAME-secrets
+	cf start $APP_NAME
+	;;
+
+esac
