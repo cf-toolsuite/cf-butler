6c0f21e7faa212ab440e5406cc3e0d9fe6a6c04d
Chris Phillipson <cphillipson@pivotal.io>
2020-09-15 17:27:50 -0700

    Remove redundant configuration

--- src/main/java/io/pivotal/cfapp/config/ButlerConfig.java
+++ src/main/java/io/pivotal/cfapp/config/ButlerConfig.java
@@ -17,22 +17,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
-import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 
 import io.pivotal.cfenv.core.CfEnv;
-import io.r2dbc.spi.ConnectionFactory;
 
 @Configuration
 public class ButlerConfig {
-
-    @Configuration
-    static class ApplicationConfiguration extends AbstractR2dbcConfiguration {
-
-		@Override
-		public ConnectionFactory connectionFactory() {
-			// TODO Auto-generated method stub
-			return null;
-		}}
 
     @Bean
     @Profile("cloud")
