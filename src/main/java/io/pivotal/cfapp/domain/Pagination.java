e3758ab1b798dc497861846337a1d4ffe6e1af20
samar singhal <samarsinghal@gmail.com>
2020-11-25 13:27:18 -0700

    Added support for pagination on metadata search endpoints  (#319)
    * ResourceMetaDataService now returns paginated results

---
+++ src/main/java/io/pivotal/cfapp/domain/Pagination.java
@@ -0,0 +1,53 @@
+package io.pivotal.cfapp.domain;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+@JsonPropertyOrder({ "pagination"})
+public class Pagination {    
+
+
+    @JsonProperty("total_results")
+    private Integer totalResult;
+
+    @JsonProperty("total_pages")
+    private Integer totalPages;
+
+
+    @JsonProperty("next")
+    private Href next;
+
+
+    @JsonProperty("previous")
+    private Href previous;
+
+
+    @JsonProperty("first")
+    private Href first;
+
+
+    @JsonProperty("last")
+    private Href last;
+
+    @JsonCreator
+    public Pagination(@JsonProperty("total_results") Integer totalResult,
+    @JsonProperty("total_pages") Integer totalPages,
+    @JsonProperty("previous") Href previous,
+    @JsonProperty("next") Href next,
+    @JsonProperty("first") Href first,
+    @JsonProperty("last") Href last
+    ) {
+        this.totalResult = totalResult;
+        this.totalPages = totalPages;
+        this.previous = previous;
+        this.next = next;
+        this.first = first;
+        this.last = last;
+    }
+}
