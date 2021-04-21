737f07f3974a339f2a8735ccc4ed20441c13289a
Chris Phillipson <cphillipson@pivotal.io>
2021-04-21 07:48:29 -0700

    Revert email notify trace logging additions

--- src/main/java/io/pivotal/cfapp/notifier/SendGridNotifier.java
+++ src/main/java/io/pivotal/cfapp/notifier/SendGridNotifier.java
@@ -55,9 +55,8 @@ public class SendGridNotifier extends EmailNotifier {
             request.setMethod(Method.POST);
             request.setEndpoint("mail/send");
             request.setBody(mail.build());
-            log.trace("About to send email to {} with subject: {}!", to.getEmail(), subject);
             Response response = sendGrid.api(request);
-            log.info("Email sent to {} with subject: {}!", to.getEmail(), subject);
+            log.info("Email sent to {} with subject: {}", to.getEmail(), subject);
             log.trace(String.format("\n\tStatus: %s\n\t%s", HttpStatus.valueOf(response.getStatusCode()).getReasonPhrase(), response.getBody()));
         } catch (IOException ioe) {
             log.warn("Could not send email!", ioe);
