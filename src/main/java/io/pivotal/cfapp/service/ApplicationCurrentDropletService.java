94460ef526588931b397e7f12c0bd472bab348c8
Chris Phillipson <cphillipson@pivotal.io>
2021-10-13 12:51:11 -0700

    PR: Refine how buildpack name and version are obtained (#391)
    * Parse buildpack version from current droplet
    * Ensure normalization of buildpack name and version remains in effect
    * For meta buildpacks, parse current droplet's buildpack info, then normalize name and version
    * Provide a new controller endpoint /apps/{guid}/droplets/current that allows for current droplet lookup by application guid
    * Updated logic in AppDetailTask
      * Noted some cases where old stopped apps (e.g., lastPushed > 30d ago) do not have buildpack name or version
      * Also, it seems appropriate to restage older stopped apps as the buildpack it may have relied upon may no longer be available or installed, see https://docs.cloudfoundry.org/devguide/deploy-apps/start-restart-restage.html#restage
    * Update dependency versions in pom.xml (e.g. Spring Boot, Sendgrid, Cloud Foundry Client)
    * Update README

---
+++ src/main/java/io/pivotal/cfapp/service/ApplicationCurrentDropletService.java
@@ -0,0 +1,65 @@
+package io.pivotal.cfapp.service;
+
+import org.cloudfoundry.reactor.DefaultConnectionContext;
+import org.cloudfoundry.reactor.TokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import io.pivotal.cfapp.config.PasSettings;
+import reactor.core.publisher.Mono;
+
+
+@Service
+// @see https://v3-apidocs.cloudfoundry.org/version/3.107.0/index.html#get-current-droplet
+public class ApplicationCurrentDropletService {
+
+    private final WebClient webClient;
+    private final DefaultConnectionContext connectionContext;
+    private final TokenProvider tokenProvider;
+    private final PasSettings settings;
+
+    @Autowired
+    public ApplicationCurrentDropletService(
+            WebClient webClient,
+            DefaultConnectionContext connectionContext,
+            TokenProvider tokenProvider,
+            PasSettings settings) {
+        this.webClient = webClient;
+        this.connectionContext = connectionContext;
+        this.tokenProvider = tokenProvider;
+        this.settings = settings;
+    }
+
+    public Mono<String> getCurrentDroplet(String guid) {
+        Assert.hasText(guid, "Global unique identifier for application instance must not be blank or null!");
+        final String uri =
+            UriComponentsBuilder
+                .newInstance()
+                .scheme("https")
+                .host(settings.getApiHost())
+                .path("/v3/apps/{guid}/droplets/current")
+                .buildAndExpand(guid)
+                .encode()
+                .toUriString();
+        return
+            getOauthToken()
+                .flatMap(t ->
+                    webClient
+                        .get()
+                        .uri(uri)
+                        .header(HttpHeaders.AUTHORIZATION, t)
+                        .retrieve()
+                        .bodyToMono(String.class)
+                );
+    }
+
+    private Mono<String> getOauthToken() {
+        tokenProvider.invalidate(connectionContext);
+        return tokenProvider.getToken(connectionContext);
+    }
+
+}
