b92365a3adea27b72d065b805d2dfc40610e91b5
Chris Phillipson <cphillipson@pivotal.io>
2023-06-21 21:53:44 -0700

    Miscellaneous updates
    * third-party dep for Collections.isEmpty replaced with custom impl of ObjectUtils.isEmpty
    * remove DROP TABLE from schema

---
+++ src/main/java/io/pivotal/cfapp/domain/ObjectUtils.java
@@ -0,0 +1,14 @@
+package io.pivotal.cfapp.domain;
+
+import java.util.Collection;
+
+public class ObjectUtils {
+
+    public static boolean isEmpty(Object[] array) {
+        return array == null || array.length == 0;
+    }
+
+    public static boolean isEmpty(Collection<?> collection) {
+        return collection == null || collection.isEmpty();
+    }
+}
