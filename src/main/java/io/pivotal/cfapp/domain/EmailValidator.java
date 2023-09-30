83d0a70771f588fd4f5df72509b8b6ed356adbd5
Rohan Nagar <rohan.nagar@utexas.edu>
2023-09-29 21:22:49 -0700

    Fix spacing

--- src/main/java/io/pivotal/cfapp/domain/EmailValidator.java
+++ src/main/java/io/pivotal/cfapp/domain/EmailValidator.java
@@ -5,6 +5,6 @@ import com.sanctionco.jmail.JMail;
 public class EmailValidator {
 
     public static boolean isValid(String email) {
-	return JMail.isValid(email);
+        return JMail.isValid(email);
     }
 }
