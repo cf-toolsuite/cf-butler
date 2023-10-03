b0b9df7c9864ab80cf0230cd51a06451880553c2
Rohan Nagar <rohan.nagar@utexas.edu>
2023-10-03 06:29:59 -0700

    Remove unnecessary blank check during email validation (#402)
    * Remove unnecessary blank check during email validation
    
    * Fix spacing

--- src/main/java/io/pivotal/cfapp/domain/EmailValidator.java
+++ src/main/java/io/pivotal/cfapp/domain/EmailValidator.java
@@ -1,17 +1,10 @@
 package io.pivotal.cfapp.domain;
 
-import org.apache.commons.lang3.StringUtils;
-
 import com.sanctionco.jmail.JMail;
 
 public class EmailValidator {
 
     public static boolean isValid(String email) {
-
-		if (StringUtils.isBlank(email)) {
-			return false;
-		}
-		return JMail.isValid(email);
-	}
-
+        return JMail.isValid(email);
+    }
 }
