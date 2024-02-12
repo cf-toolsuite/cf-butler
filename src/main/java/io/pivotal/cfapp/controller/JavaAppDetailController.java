999a063ec291db11a916b7098791468eb73bb301
Chris Phillipson <cphillipson@pivotal.io>
2024-02-12 15:44:43 -0800

    Reworked /download/pomfiles endpoint impl
    * Reduced complexity and much easier to understand
    * No need for extra wrapping and input stream

--- src/main/java/io/pivotal/cfapp/controller/JavaAppDetailController.java
+++ src/main/java/io/pivotal/cfapp/controller/JavaAppDetailController.java
@@ -1,12 +1,7 @@
 package io.pivotal.cfapp.controller;
 
-import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
@@ -15,7 +10,6 @@ import java.util.List;
 
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
-import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DefaultDataBufferFactory;
@@ -50,7 +44,7 @@ public class JavaAppDetailController {
             service
                 .findAll()
                 .collectList()
-                .flatMapMany(list -> createTarGz(list, filename));
+                .flatMapMany(list -> createTarGz(list));
         return
             ResponseEntity
                 .ok()
@@ -59,48 +53,30 @@ public class JavaAppDetailController {
                 .body(tarball);
     }
 
-    private static Flux<DataBuffer> createTarGz(List<JavaAppDetail> appDetails, String outputFileName) {
+    private static Flux<DataBuffer> createTarGz(List<JavaAppDetail> appDetails) {
         Flux<DataBuffer> result = Flux.empty();
-        try {
-            Path tarFilePath = Files.createTempFile("apps", ".tar");
-            try (TarArchiveOutputStream tarOs =
-                new TarArchiveOutputStream(
-                    new GzipCompressorOutputStream(new BufferedOutputStream(new FileOutputStream(tarFilePath.toFile()))))) {
-                for (JavaAppDetail appDetail : appDetails) {
-                    if (appDetail.getPomContents() != null && !appDetail.getPomContents().isEmpty()) {
-                        String dirPath = String.format("%s/%s/%s", appDetail.getOrganization(), appDetail.getSpace(), appDetail.getAppName());
-                        Path entryPath = Paths.get(dirPath, "pom.xml");
-                        TarArchiveEntry entry = new TarArchiveEntry(entryPath.toString());
-                        byte[] contentBytes = appDetail.getPomContents().getBytes();
-                        entry.setSize(contentBytes.length);
-                        tarOs.putArchiveEntry(entry);
-                        tarOs.write(contentBytes);
-                        tarOs.closeArchiveEntry();
-                    }
+        try (
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            TarArchiveOutputStream tarOutput = new TarArchiveOutputStream(byteArrayOutputStream)) {
+            for (JavaAppDetail appDetail : appDetails) {
+                if (appDetail.getPomContents() != null && !appDetail.getPomContents().isEmpty()) {
+                    String dirPath = String.format("%s/%s/%s", appDetail.getOrganization(), appDetail.getSpace(), appDetail.getAppName());
+                    Path entryPath = Paths.get(dirPath, "pom.xml");
+                    TarArchiveEntry entry = new TarArchiveEntry(entryPath.toString());
+                    byte[] contentBytes = appDetail.getPomContents().getBytes();
+                    entry.setSize(contentBytes.length);
+                    tarOutput.putArchiveEntry(entry);
+                    tarOutput.write(contentBytes);
+                    tarOutput.closeArchiveEntry();
                 }
-                tarOs.finish();
-
-                File tarball = Files.createTempFile("", outputFileName).toFile();
-
-                try (FileInputStream fis = new FileInputStream(tarball);
-                    ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-                    byte[] buffer = new byte[1024];
-                    int bytesRead;
-                    while ((bytesRead = fis.read(buffer)) != -1) {
-                        baos.write(buffer, 0, bytesRead);
-                    }
-                    byte[] bytes = baos.toByteArray();
-                    DefaultDataBufferFactory factory = new DefaultDataBufferFactory();
-                    DataBuffer db = factory.wrap(bytes);
-                    result = Flux.just(db);
-                } catch (IOException ioe) {
-                    log.error("Could not create Flux<DataBuffer>", ioe);
-                }
-            } catch (IOException ioe) {
-                log.error("Could not create TAR output stream", ioe);
             }
+            tarOutput.finish();
+            byte[] tarBytes = byteArrayOutputStream.toByteArray();
+            DefaultDataBufferFactory factory = new DefaultDataBufferFactory();
+            DataBuffer db = factory.wrap(tarBytes);
+            result = Flux.just(db);
         } catch (IOException ioe) {
-            log.error("Could not create temporary file named app.tar", ioe);
+            log.error("Could not create Flux<DataBuffer>", ioe);
         }
         return result;
     }
