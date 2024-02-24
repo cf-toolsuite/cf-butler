833b94ca715c7d2543b863ca9683b1a8b1ebf507
Chris Phillipson <cphillipson@pivotal.io>
2024-02-24 14:10:59 -0800

    Improve TgzUtil
    * collapse method implementation
    * make certain to close DataBuffer inputStream

--- src/main/java/io/pivotal/cfapp/util/TgzUtil.java
+++ src/main/java/io/pivotal/cfapp/util/TgzUtil.java
@@ -1,6 +1,5 @@
 package io.pivotal.cfapp.util;
 
-import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -25,38 +24,37 @@ public class TgzUtil {
             DataBufferUtils
                 .join(dataBufferFlux)
                 .flatMap(dataBuffer -> {
-                    try {
-                        InputStream is = new BufferedInputStream(dataBuffer.asInputStream(true)); // true for releasing the buffer
-                        return extractFromTarGz(is, filename);
+                    Mono<String> result = Mono.empty();
+                    try (
+                        InputStream inputStream = dataBuffer.asInputStream(true); // true for releasing the buffer
+                        GzipCompressorInputStream gzipInputStream = new GzipCompressorInputStream(inputStream);
+                        TarArchiveInputStream tarArchiveInputStream = new TarArchiveInputStream(gzipInputStream)
+                        )
+                    {
+                        TarArchiveEntry entry;
+                        while ((entry = (TarArchiveEntry) tarArchiveInputStream.getNextEntry()) != null) {
+                            if (entry.getName().endsWith(filename)) {
+                                StringBuilder contentBuilder = new StringBuilder();
+                                byte[] buffer = new byte[1024];
+                                int len;
+                                while ((len = tarArchiveInputStream.read(buffer)) != -1) {
+                                    contentBuilder.append(new String(buffer, 0, len, StandardCharsets.UTF_8));
+                                }
+                                result = Mono.just(contentBuilder.toString());
+                                break;
+                            }
+                        }
+                    } catch (IOException ioe) {
+                        result = Mono.error(ioe);
                     } finally {
                         DataBufferUtils.release(dataBuffer);
                     }
+                    return result;
                 });
     }
 
-    private static Mono<String> extractFromTarGz(InputStream inputStream, String filename) {
-        try (GzipCompressorInputStream gzipInputStream = new GzipCompressorInputStream(inputStream);
-            TarArchiveInputStream tarArchiveInputStream = new TarArchiveInputStream(gzipInputStream)) {
-            TarArchiveEntry entry;
-            while ((entry = (TarArchiveEntry) tarArchiveInputStream.getNextEntry()) != null) {
-                if (entry.getName().endsWith(filename)) {
-                    StringBuilder contentBuilder = new StringBuilder();
-                    byte[] buffer = new byte[1024];
-                    int len;
-                    while ((len = tarArchiveInputStream.read(buffer)) != -1) {
-                        contentBuilder.append(new String(buffer, 0, len, StandardCharsets.UTF_8));
-                    }
-                    return Mono.just(contentBuilder.toString());
-                }
-            }
-        } catch (IOException e) {
-            return Mono.error(e);
-        }
-        return Mono.empty();
-    }
-
     public static void main(String[] args) {
-        //File droplet = new File("/tmp/droplet_00b473e8-eedf-4243-be14-4dce6cdd88f7.tgz");
+        //File droplet = new File("/tmp/droplet_bc11a63f-36ed-423f-9c4b-eb57f0b0cd40.tgz");
         File droplet = new File(args[0]);
         if (droplet.exists()) {
             try (FileInputStream fis = new FileInputStream(droplet);
