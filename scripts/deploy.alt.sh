355c67c7829978ea25a83fd6ba8ad37c762f58cb
Chris Phillipson <cphillipson@pivotal.io>
2024-04-13 10:09:38 -0700

    Faciliate accepting additional push options on deploy

--- scripts/deploy.alt.sh
+++ scripts/deploy.alt.sh
@@ -5,7 +5,7 @@ set -e
 export APP_NAME=cf-butler
 
 if [ -z "$1" ] && [ -z "$2" ]; then
-  echo "Usage: deploy.alt.sh {credential_store_provider_option} {path_to_secrets_file}"
+  echo "Usage: deploy.alt.sh {credential_store_provider_option} {path_to_secrets_file} {additional-cf-push-options}"
   exit 1
 fi
 
@@ -14,12 +14,16 @@ fi
 case "$1" in
 
   --with-credhub | -c)
-  cf push --no-start
+  cf push --no-start "$3"
   if ! cf service $APP_NAME-secrets > /dev/null; then
     cf create-service credhub default $APP_NAME-secrets -c "$2"
-    while [[ $(cf service $APP_NAME-secrets) != *"succeeded"* ]]; do
-      echo "$APP_NAME-secrets is not ready yet..."
-      sleep 5
+    for (( i = 0; i < 90; i++ )); do
+      if [[ $(cf service $APP_NAME-secrets) != *"succeeded"* ]]; then
+        echo "$APP_NAME-secrets is not ready yet..."
+        sleep 10
+      else
+        break
+      fi
     done
   fi
   cf bind-service $APP_NAME $APP_NAME-secrets
@@ -27,12 +31,16 @@ case "$1" in
   ;;
 
   _ | *)
-  cf push --no-start
+  cf push --no-start "$3"
   if ! cf service $APP_NAME-secrets > /dev/null; then
     cf create-user-provided-service $APP_NAME-secrets -p "$2"
-    while [[ $(cf service $APP_NAME-secrets) != *"succeeded"* ]]; do
-      echo "$APP_NAME-secrets is not ready yet..."
-      sleep 5
+    for (( i = 0; i < 90; i++ )); do
+      if [[ $(cf service $APP_NAME-secrets) != *"succeeded"* ]]; then
+        echo "$APP_NAME-secrets is not ready yet..."
+        sleep 10
+      else
+        break
+      fi
     done
   fi
   cf bind-service $APP_NAME $APP_NAME-secrets
