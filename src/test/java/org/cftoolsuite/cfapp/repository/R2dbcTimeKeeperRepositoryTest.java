8fcc0080ff0e4b853e8bc08cca71d18498bb0e56
Chris Phillipson <christopher.a.phillipson@gmail.com>
2025-06-27 10:52:07 -0700

    Fix compilation issues * update dependency version * revive missing static imports in test classes

--- src/test/java/org/cftoolsuite/cfapp/repository/R2dbcTimeKeeperRepositoryTest.java
+++ src/test/java/org/cftoolsuite/cfapp/repository/R2dbcTimeKeeperRepositoryTest.java
@@ -1,20 +1,16 @@
 package org.cftoolsuite.cfapp.repository;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.time.LocalDateTime;
-
 import org.cftoolsuite.cfapp.ButlerTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 import reactor.test.StepVerifier;
 
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @ButlerTest
-@ExtendWith(SpringExtension.class)
 public class R2dbcTimeKeeperRepositoryTest {
 
     private final R2dbcTimeKeeperRepository repo;
